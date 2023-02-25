5d1200ae439b062d4bb7459df712ed5517a11f79
Daiyan Rafi <daiyanrafi1@gmail.com>
2023-02-25 20:02:45 +0600

    2502 2002

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BookModule } from './book/book.module';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
-  imports: [ BookModule ],
+  imports: [ BookModule, UsersModule ],
   controllers: [],
   providers: [],
 })
